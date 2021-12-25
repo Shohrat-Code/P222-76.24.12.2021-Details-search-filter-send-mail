ae3a2d5be7787ca7458da911602a5b9432561c1a
Shohrat-Code <shohrat@code.edu.az>
2021-12-25 16:31:13 +0400

    Codes

---
+++ azen/azen/Areas/admin/Controllers/SubscribeController.cs
@@ -0,0 +1,58 @@
+﻿using azen.Data;
+using azen.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace azen.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class SubscribeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SubscribeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Subscribes.ToList());
+        }
+
+        public IActionResult SendMailAll()
+        {
+            return View(_context.Subscribes.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult SendMailAll(string MailText)
+        {
+            List<Subscribe> subscribes = _context.Subscribes.ToList();
+
+            foreach (var item in subscribes)
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("codegroupsp@gmail.com", "Code academy p222");
+                mail.To.Add(item.Email);
+                mail.Body = MailText;
+                mail.IsBodyHtml = true;
+                mail.Subject = "Reklam";
+
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.EnableSsl = true;
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new NetworkCredential("codegroupsp@gmail.com", "codegroupsp2021");
+
+                smtpClient.Send(mail);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ azen/azen/Controllers/BlogController.cs
@@ -0,0 +1,57 @@
+﻿using azen.Data;
+using azen.Models;
+using azen.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace azen.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BlogController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(VmSearch search)
+        {
+            VmBlog model = new VmBlog();
+
+            //if (tagId == null)
+            //{
+            //    blogs = _context.Blogs.Include(u => u.CustomUser).Include(c => c.BlogCategory).ToList();
+            //}
+            //else
+            //{
+            //    blogs = _context.Blogs
+            //                          .Include(u => u.CustomUser)
+            //                          .Include(c => c.BlogCategory)
+            //                          .Where(b => b.TagToBlogs.Any(tb => tb.TagId == tagId))
+            //                          .ToList();
+            //}
+
+            model.Blogs= _context.Blogs
+                                      .Include(u => u.CustomUser)
+                                      .Include(c => c.BlogCategory)
+                                      .Where(b => (search.tagId != null ? b.TagToBlogs.Any(tb => tb.TagId == search.tagId) : true) &&
+                                                  (search.catId != null ? b.BlogCategoryId == search.catId : true) &&
+                                                  (search.searchData != null ? b.Title.Contains(search.searchData) : true)
+                                      ).ToList();
+            model.Search = search;
+            model.BlogCategories = _context.BlogCategories.ToList();
+            model.Tags = _context.Tags.ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_context.Blogs.Include(u => u.CustomUser).Include(c => c.BlogCategory).Include(tb => tb.TagToBlogs).ThenInclude(t => t.Tag).FirstOrDefault(p => p.Id == id));
+        }
+    }
+}
